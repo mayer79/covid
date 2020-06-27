f884592e1f8d44d1ffb8381d031e836695849aeb
mayer79 <mayermichael79@gmail.com>
2020-06-27 16:00:52 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# covid
-Analyses on COVID-19
+# COVID-19 Cases per Mio
+
+The short R script "cases_per_mio.R" in subfolder "r" 
+
+1. downloads data from [European Centre for Disease Prevention and Control](https://www.ecdc.europa.eu/en),
+
+2. selects a couple of countries and does the necessary transformations, and
+
+3. plots the cases per mio inhabitants including a scatter plot smoother.
+
+The resulting plot as per June 27, 2020 is as follows:
+
+![](cases_per_mio.png)
---
+++ r/cases_per_mio.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(lubridate)
+
+# Source: European Centre for Disease Prevention and Control https://www.ecdc.europa.eu/en
+raw <- read_csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv")
+
+# Transformations and filters
+prep <- raw %>%
+  mutate(Date = dmy(dateRep),
+         `Cases per Mio` = cases / popData2019 * 1e6)  %>%
+  filter(Date >= "2020-03-01",
+         countriesAndTerritories %in% c("Switzerland", "United_States_of_America", "Germany", "Sweden"))
+
+# Plot
+ggplot(prep, aes(x = Date, y = `Cases per Mio`)) +
+  geom_line() +
+  geom_smooth() +
+  facet_wrap(~ countriesAndTerritories)
